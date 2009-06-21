96723744b3482c0245604272dcaa23ce18158327
arnestig <arnestig@b4d8fe5f-8a90-43c9-9084-dcb629e63849>
2009-06-21 19:49:30 +0000

    Changed declaration of ShadowMap.
    Now it uses sEnvironmentMap, mostly because sEnvironmentMap is equal to sShadowMap today.

--- src/CZone.h
+++ src/CZone.h
@@ -28,11 +28,9 @@ struct sTileMap {
 
 struct sEnvironmentMap {
     int x_pos, y_pos, id;
-    float transparency;
-    bool walkable;
-    SDL_Rect CR; // collision rectangle
-    sEnvironmentMap( int _x, int _y, int _tex_id, bool _walkable, int _CR_x, int _CR_y, int _CR_h, int _CR_w) {
-        x_pos = _x; y_pos = _y; id = _tex_id; walkable = _walkable; CR.x = _CR_x; CR.y = _CR_y; CR.h = _CR_h; CR.w = _CR_w; transparency = 1.0f;
+    float transparency, red, green, blue;
+    sEnvironmentMap(int _x, int _y, int _tex_id, float _tp, float _red, float _green, float _blue) {
+        x_pos = _x; y_pos = _y; id = _tex_id; transparency = _tp; red = _red; green = _green; blue = _blue;
     };
 };
 
@@ -43,8 +41,10 @@ struct sCollisionMap {
 
 struct sShadowMap {
     int x_pos, y_pos, id;
-    float transparency;
-    sShadowMap( int _x, int _y, int _tex_id) { x_pos = _x; y_pos = _y; id = _tex_id; transparency = 1.0f; };
+    float transparency, red, green, blue;
+    sShadowMap(int _x, int _y, int _tex_id, float _tp, float _red, float _green, float _blue) {
+        x_pos = _x; y_pos = _y; id = _tex_id; transparency = _tp; red = _red; green = _green; blue = _blue;
+    };
 };
 
 class CZone {
@@ -76,7 +76,8 @@ class CZone {
 
     std::vector<sTileMap> TileMap;
     std::vector<sEnvironmentMap> EnvironmentMap;
-    std::vector<sShadowMap> ShadowMap;
+    // this is the old shadowmap, keeping it here a while... std::vector<sShadowMap> ShadowMap;
+    std::vector<sEnvironmentMap> ShadowMap;
     std::vector<sCollisionMap> CollisionMap;
 
     CTexture ZoneTiles;
