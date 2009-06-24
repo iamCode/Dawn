c5a8cd2f605c6fef72733ef9248cada92371ff9c
arnestig <arnestig@b4d8fe5f-8a90-43c9-9084-dcb629e63849>
2009-06-24 18:01:25 +0000

    Added CMessage class.

--- src/CMessage.cpp
+++ src/CMessage.cpp
@@ -1,1 +1,70 @@
+/**                    Dawn - 2D roleplaying game
+    Copyright (C) 2009  Tobias Eliasson (arnestig@users.sourceforge.net)
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. **/
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "CMessage.h"
+
+void CMessage::DrawAll() {
+    GLFT_Font fnt("data/verdana.ttf", 12);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+
+    for (unsigned int book = 0; book < MessageBook.size(); book++) {
+        if (MessageBook[book].lifetime > 0.0) { // if we have a lifetime, we need to wait until we start decaying the message.
+            MessageBook[book].startdecaythisframe = SDL_GetTicks();
+            // currently waiting 1 second, until we decrease our lifetime with one second and then repeat.
+            if ((MessageBook[book].startdecaythisframe-MessageBook[book].startdecaylastframe) > 1000) {
+                MessageBook[book].startdecaylastframe = MessageBook[book].startdecaythisframe;
+                MessageBook[book].lifetime -= 1.0;
+            }
+        }
+        if (MessageBook[book].lifetime == 0.0) { // lifetime is up, lets start decaying...
+            MessageBook[book].thisframe = SDL_GetTicks();
+            if ((MessageBook[book].thisframe-MessageBook[book].lastframe) > MessageBook[book].decayrate) {
+                MessageBook[book].lastframe = MessageBook[book].thisframe;
+                MessageBook[book].transparency -= 0.01;
+            }
+        }
+        // set the color + transparency then draw the text.
+        glColor4f(MessageBook[book].red,MessageBook[book].green,MessageBook[book].blue,MessageBook[book].transparency);
+        fnt.drawText(MessageBook[book].x_pos,MessageBook[book].y_pos,MessageBook[book].string);
+    }
+    glDisable(GL_BLEND);
+    glColor4f(1.0f,1.0f,1.0f,1.0f);
+};
+
+void CMessage::AddText(int x, int y, float red, float green, float blue, float transparency, int decayrate, float lifetime, char *text, ...) {
+    char *buffer;
+    std::va_list args;
+
+    // writing our text and arguments to the buffer
+    va_start(args, text);
+    vsprintf(buffer,text,args);
+    va_end(args);
+
+    // push everything to our vector.
+    MessageBook.push_back(sBook(buffer, x, y, red, green, blue, transparency, decayrate, lifetime));
+};
+
+void CMessage::DeleteDecayed() {
+    for (unsigned int book = 0; book < MessageBook.size(); book++) {
+        if (MessageBook[book].transparency < 0.0) {
+            MessageBook.erase(MessageBook.begin()+book);
+        }
+    }
+};
--- src/CMessage.h
+++ src/CMessage.h
@@ -1,1 +1,48 @@
+/**                    Dawn - 2D roleplaying game
+    Copyright (C) 2009  Tobias Eliasson (arnestig@users.sourceforge.net)
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. **/
+
+#ifndef __C_MESSAGE__
+#define __C_MESSAGE__
+
+#include "GLFT_Font.h"
+#include <SDL/SDL.h>
+
+
+struct sBook {
+    char *string;
+    float red, green, blue, transparency;
+    float lifetime;
+    int decayrate;
+    int x_pos, y_pos;
+    float lastframe, thisframe;
+    float startdecaylastframe, startdecaythisframe;
+
+    sBook (char *_text, int _x_pos, int _y_pos, float _red, float _green, float _blue, float _transparency, int _decayrate, float _lifetime) {
+        string = _text; x_pos = _x_pos; y_pos = _y_pos; red = _red; green = _green; blue = _blue; transparency = _transparency; decayrate = _decayrate; lifetime = _lifetime; thisframe = 0.0f; lastframe = 0.0f; startdecaylastframe = 0.0f; startdecaythisframe = 0.0f;
+    };
+};
+
+class CMessage {
+    public:
+    void AddText(int x, int y, float red, float green, float blue, float transparency, int decayrate, float lifetime, char *text, ...);
+    void DrawAll();
+    void DeleteDecayed();
+
+    private:
+    std::vector<sBook> MessageBook;
+};
+
+#endif
