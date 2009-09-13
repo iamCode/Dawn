716960bf4247e20a1b4b91078e7338f241a92fc5
arnestig <arnestig@b4d8fe5f-8a90-43c9-9084-dcb629e63849>
2009-09-13 20:43:57 +0000

    Added missing GPLv3 licence.

--- src/debug.cpp
+++ src/debug.cpp
@@ -1,3 +1,21 @@
+/**
+    Copyright (C) 2009  Dawn - 2D roleplaying game
+
+    This file is a part of the dawn-rpg project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. **/
+
 #include "debug.h"
 
 /* The init values are the class
@@ -6,7 +24,7 @@ namespace dawn_configuration {
 	std::string logfile = "dawn-log.cpp"; // The logfile
 	bool debug_stdout = true; // Write messages to stdout
 	bool debug_fileout = true; // Write messages to a file
-	bool show_info_messages = true; // Show or hide debug info	
+	bool show_info_messages = true; // Show or hide debug info
 	bool show_warn_messages = true; // show or hide warnings
 }
 
@@ -20,7 +38,7 @@ static std::string date_time_string()
 	time_t t = time(0);
 	strftime(buf, _stringmax, "%X", localtime(&t));
 	_datetime = buf;
-	
+
 	return _datetime;
 }
 
@@ -33,18 +51,18 @@ void dawn_debug_info(const std::string& message)
 			log_started = true;
 			temp.close();
 		}
-		
+
 		std::ofstream outputfile(dawn_configuration::logfile.c_str(), std::ios_base::app);
 
 		if(dawn_configuration::debug_stdout)
-			std::cout << date_time_string() << ": Information : " << 
-				message << std::endl;	
+			std::cout << date_time_string() << ": Information : " <<
+				message << std::endl;
 		if(dawn_configuration::debug_fileout)
 			if(outputfile)
-				outputfile << date_time_string() << ": Information : " << 
+				outputfile << date_time_string() << ": Information : " <<
 					message << std::endl;
 
-		outputfile.close(); 
+		outputfile.close();
 	}
 }
 
@@ -59,16 +77,16 @@ void dawn_debug_warn(const std::string& message)
 		}
 
 		std::ofstream outputfile(dawn_configuration::logfile.c_str(), std::ios_base::app);
-		
+
 		if(dawn_configuration::debug_stdout)
-			std::cout << date_time_string() << ": Warning : " << 
-				message << std::endl;	
+			std::cout << date_time_string() << ": Warning : " <<
+				message << std::endl;
 		if(dawn_configuration::debug_fileout)
 			if(outputfile)
-				outputfile << date_time_string() << ": Warning : " << 
+				outputfile << date_time_string() << ": Warning : " <<
 					message << std::endl;
-	
-	outputfile.close(); 
+
+	outputfile.close();
 	}
 }
 
@@ -82,17 +100,17 @@ void dawn_debug_fatal(const std::string& message)
 	}
 
 	std::ofstream outputfile(dawn_configuration::logfile.c_str(), std::ios_base::app);
-	
+
 	if(dawn_configuration::debug_stdout)
-		std::cout << date_time_string() << 
-			": Fatal Error : " << message << 
+		std::cout << date_time_string() <<
+			": Fatal Error : " << message <<
 			std::endl;
 	if(dawn_configuration::debug_fileout)
 		if(outputfile)
-			outputfile << date_time_string() << 
-				": Fatal Error : "<< message << 
+			outputfile << date_time_string() <<
+				": Fatal Error : "<< message <<
 				std::endl;
 
 	outputfile.close();
-	exit(1); 
+	exit(1);
 }
--- src/debug.h
+++ src/debug.h
@@ -1,3 +1,21 @@
+/**
+    Copyright (C) 2009  Dawn - 2D roleplaying game
+
+    This file is a part of the dawn-rpg project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. **/
+
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
@@ -7,21 +25,21 @@
 #include <cstdlib>
 #include <vector>
 
-static bool log_started = false; /* This gets set to true 
-	after the first write (so that the log file is 
+static bool log_started = false; /* This gets set to true
+	after the first write (so that the log file is
 	cleared on the the first log) */
 
 namespace dawn_configuration {
 	extern std::string logfile; // The logfile
 	extern bool debug_stdout; // Write messages to stdout
 	extern bool debug_fileout; // Write messages to a file
-	extern bool show_info_messages; // Show or hide debug info	
+	extern bool show_info_messages; // Show or hide debug info
 	extern bool show_warn_messages; // show or hide warnings
 }
 
-void dawn_debug_info(const std::string& message); /* Logs useful 
+void dawn_debug_info(const std::string& message); /* Logs useful
 						debug data */
-void dawn_debug_warn(const std::string& message); /* Logs non fatal 
+void dawn_debug_warn(const std::string& message); /* Logs non fatal
 						problems */
 void dawn_debug_fatal(const std::string& message); /* Logs fatal
 						problems then quits */
