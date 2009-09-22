f23da94972fc66c68d8281c4994222c93d87dab4
arnestig <arnestig@b4d8fe5f-8a90-43c9-9084-dcb629e63849>
2009-09-22 15:06:23 +0000

    Added GPLv3 licence.

--- src/CharacterInfoScreen.h
+++ src/CharacterInfoScreen.h
@@ -1,3 +1,21 @@
+/**
+    Copyright (C) 2009  Dawn - 2D roleplaying game
+
+    This file is a part of the dawn-rpg project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. **/
+
 #ifndef _CharacterInfoScreen_h_
 #define _CharacterInfoScreen_h_
 
@@ -18,13 +36,13 @@ class CharacterInfoScreen
 	public:
 		CharacterInfoScreen( Player *player_ );
 		~CharacterInfoScreen();
-		
+
 		void setVisible( bool newVisible );
 		bool isVisible() const;
-		
+
 		void LoadTextures();
 		void initFonts();
-		
+
 		void drawScreen();
 };
 
