5a06e8da94e5ac21214759602aaae75af467b284
zaldron <zaldron@b4d8fe5f-8a90-43c9-9084-dcb629e63849>
2009-10-02 19:46:24 +0000

    Allowed picking up and dropping items and moving items around in the inventory. Equipping items is not possible yet

---
+++ src/item.cpp
@@ -0,0 +1,48 @@
+/**
+    Copyright (C) 2009  Dawn - 2D roleplaying game
+
+    This file is a part of the dawn-rpg project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. **/
+
+#include "item.h"
+
+Item::Item( std::string name_, size_t sizeX_, size_t sizeY_, std::string symbolFile )
+	:	name( name_ ),
+		sizeX( sizeX_ ),
+		sizeY( sizeY_ )
+{
+	symbolTexture.texture.reserve(1);
+	symbolTexture.LoadIMG( symbolFile, 0 );
+}
+
+std::string Item::getName() const
+{
+	return name;
+}
+
+size_t Item::getSizeX() const
+{
+	return sizeX;
+}
+
+size_t Item::getSizeY() const
+{
+	return sizeY;
+}
+
+CTexture* Item::getSymbolTexture()
+{
+	return &symbolTexture;
+}
