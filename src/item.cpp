2f3d85741ad9998c2d55dc3b58c46b4fcfa0e3ef
arnestig <arnestig@b4d8fe5f-8a90-43c9-9084-dcb629e63849>
2009-10-16 21:03:06 +0000

    Added new functions to the item class. Stats, description, enums and much more.

--- src/item.cpp
+++ src/item.cpp
@@ -18,11 +18,31 @@
 
 #include "item.h"
 
-Item::Item( std::string name_, size_t sizeX_, size_t sizeY_, std::string symbolFile, EquipPosition::EquipPosition equipPosition_ )
+Item::Item( std::string name_, size_t sizeX_, size_t sizeY_, std::string symbolFile,
+        ItemQuality::ItemQuality itemQuality_,
+        EquipPosition::EquipPosition equipPosition_,
+        ItemType::ItemType itemType_,
+        ArmorType::ArmorType armorType_,
+        WeaponType::WeaponType weaponType_ )
 	:	name( name_ ),
 		sizeX( sizeX_ ),
 		sizeY( sizeY_ ),
-		equipPosition( equipPosition_ )
+		armorModifier( 0 ),
+		minDamage( 0 ),
+		maxDamage( 0 ),
+		dexterityModifier( 0 ),
+		intellectModifier( 0 ),
+		strengthModifier( 0 ),
+		vitalityModifier( 0 ),
+		wisdomModifier( 0 ),
+		healthModifier( 0 ),
+		manaModifier( 0 ),
+		energyModifier( 0 ),
+		itemQuality ( itemQuality_ ),
+		equipPosition ( equipPosition_ ),
+		itemType ( itemType_ ),
+		armorType ( armorType_ ),
+		weaponType ( weaponType_ )
 {
 	symbolTexture.texture.reserve(1);
 	symbolTexture.LoadIMG( symbolFile, 0 );
@@ -48,9 +68,261 @@ CTexture* Item::getSymbolTexture()
 	return &symbolTexture;
 }
 
+ItemQuality::ItemQuality Item::getItemQuality() const
+{
+    return itemQuality;
+}
+
+ItemSlot::ItemSlot Item::getItemSlot() const
+{
+    return itemSlot;
+}
+
+ItemType::ItemType Item::getItemType() const
+{
+    return itemType;
+}
+
+ArmorType::ArmorType Item::getArmorType() const
+{
+    return armorType;
+}
+
+WeaponType::WeaponType Item::getWeaponType() const
+{
+    return weaponType;
+}
+
 EquipPosition::EquipPosition Item::getEquipPosition() const
 {
-	return equipPosition;
+    return equipPosition;
+}
+
+std::string Item::getDescription() const
+{
+    return description;
+}
+
+int16_t Item::getStrength() const
+{
+    return strengthModifier;
+}
+
+int16_t Item::getDexterity() const
+{
+    return dexterityModifier;
+}
+
+int16_t Item::getIntellect() const
+{
+    return intellectModifier;
+}
+
+int16_t Item::getVitality() const
+{
+    return vitalityModifier;
+}
+
+int16_t Item::getWisdom() const
+{
+    return wisdomModifier;
+}
+
+int16_t Item::getHealth() const
+{
+    return healthModifier;
+}
+
+int16_t Item::getMana() const
+{
+    return manaModifier;
+}
+
+int16_t Item::getEnergy() const
+{
+    return energyModifier;
+}
+
+int16_t Item::getArmor() const
+{
+    return armorModifier;
+}
+
+uint8_t Item::getMinDamage() const
+{
+    return minDamage;
+}
+
+uint8_t Item::getMaxDamage() const
+{
+    return maxDamage;
+}
+
+void Item::setStrength( int16_t strength )
+{
+    strengthModifier = strength;
+}
+
+void Item::setDexterity( int16_t dexterity )
+{
+    dexterityModifier = dexterity;
 }
 
 
+void Item::setIntellect( int16_t intellect )
+{
+    intellectModifier = intellect;
+}
+
+void Item::setVitality( int16_t vitality )
+{
+    vitalityModifier = vitality;
+}
+
+void Item::setWisdom( int16_t wisdom )
+{
+    wisdomModifier = wisdom;
+}
+
+void Item::setHealth( int16_t health )
+{
+    healthModifier = health;
+}
+
+void Item::setMana( int16_t mana )
+{
+    manaModifier = mana;
+}
+
+void Item::setEnergy( int16_t energy )
+{
+    energyModifier = energy;
+}
+
+void Item::setArmor( int16_t armor )
+{
+    armorModifier = armor;
+}
+
+void Item::setMinDamage( uint8_t minDamage_ )
+{
+    minDamage = minDamage_;
+}
+
+void Item::setMaxDamage( uint8_t maxDamage_ )
+{
+    maxDamage = maxDamage_;
+}
+
+void Item::setDescription ( std::string description_ )
+{
+    description = "\"" + description_ + "\"";
+}
+
+std::string Item::getArmorTypeText() const
+{
+    switch ( armorType )
+    {
+        case ArmorType::CLOTH:
+            return "Cloth";
+        break;
+        case ArmorType::LEATHER:
+            return "Leather";
+        break;
+        case ArmorType::MAIL:
+            return "Mail";
+        break;
+        case ArmorType::PLATE:
+            return "Plate";
+        break;
+    }
+}
+
+std::string Item::getWeaponTypeText() const
+{
+    switch ( weaponType )
+    {
+        case WeaponType::ONEHAND_SWORD:
+            return "One handed sword";
+        break;
+        case WeaponType::TWOHAND_SWORD:
+            return "Two handed sword";
+        break;
+        case WeaponType::DAGGER:
+            return "Dagger";
+        break;
+        case WeaponType::STAFF:
+            return "Staff";
+        break;
+        case WeaponType::ONEHAND_CLUB:
+            return "One handed club";
+        break;
+        case WeaponType::TWOHAND_CLUB:
+            return "Two handed club";
+        break;
+        case WeaponType::WAND:
+            return "Wand";
+        break;
+        case WeaponType::SPELLBOOK:
+            return "Spellbook";
+        break;
+        case WeaponType::ONEHAND_MACE:
+            return "One handed mace";
+        break;
+        case WeaponType::TWOHAND_MACE:
+            return "Two handed mace";
+        break;
+        case WeaponType::BOW:
+            return "Bow";
+        break;
+        case WeaponType::SLINGSHOT:
+            return "Slingshot";
+        break;
+        case WeaponType::CROSSBOW:
+            return "Crossbow";
+        break;
+        case WeaponType::SHIELD:
+            return "Shield";
+        break;
+    }
+}
+
+std::string Item::getEquipPositionText() const
+{
+    switch ( itemSlot )
+    {
+        case EquipPosition::MAIN_HAND:
+            return "Main hand";
+        break;
+        case EquipPosition::OFF_HAND:
+            return "Off hand";
+        break;
+        case EquipPosition::CHEST:
+            return "Chest";
+        break;
+        case EquipPosition::LEGS:
+            return "Legs";
+        break;
+        case EquipPosition::BOOTS:
+            return "Boots";
+        break;
+        case EquipPosition::RING:
+            return "Ring";
+        break;
+        case EquipPosition::GLOVES:
+            return "Gloves";
+        break;
+        case EquipPosition::HEAD:
+            return "Head";
+        break;
+        case EquipPosition::CLOAK:
+            return "Cloak";
+        break;
+        case EquipPosition::AMULET:
+            return "Amulet";
+        break;
+        case EquipPosition::SHOULDER:
+            return "Shoulder";
+        break;
+    }
+}
--- src/item.h
+++ src/item.h
@@ -24,68 +24,190 @@
 
 namespace ItemSlot
 {
-	enum ItemSlot
-	{
-		HEAD,
-		AMULET,
-		MAIN_HAND,
-		OFF_HAND,
-		BELT,
-		LEGGING,
-		SHOULDER,
-		CHEST,
-		GLOVES,
-		CLOAK,
-		BOOTS,
-		RING_ONE,
-		RING_TWO,
-		COUNT // this is always the las and gives the size of the enum
-	};
+    enum ItemSlot
+    {
+        HEAD,
+        AMULET,
+        MAIN_HAND,
+        OFF_HAND,
+        BELT,
+        LEGS,
+        SHOULDER,
+        CHEST,
+        GLOVES,
+        CLOAK,
+        BOOTS,
+        RING_ONE,
+        RING_TWO,
+        COUNT // this is always the last and gives the size of the enum
+    };
 }
 
 namespace EquipPosition
 {
-	enum EquipPosition
-	{
-		HEAD,
-		AMULET,
-		MAIN_HAND,
-		OFF_HAND,
-		BELT,
-		LEGGING,
-		SHOULDER,
-		CHEST,
-		GLOVES,
-		CLOAK,
-		BOOTS,
-		RING,
-		NONE
-	};
+    enum EquipPosition
+    {
+        MAIN_HAND,
+        OFF_HAND,
+        CHEST,
+        LEGS,
+        BELT,
+        BOOTS,
+        RING,
+        GLOVES,
+        HEAD,
+        CLOAK,
+        AMULET,
+        SHOULDER,
+        NONE
+    };
+}
+
+namespace ItemType
+{
+    enum ItemType
+    {
+        QUESTITEM,
+        MISCELLANEOUS,
+        ARMOR,
+        WEAPON,
+        JEWELRY,
+        COUNT
+    };
+}
+
+namespace ArmorType
+{
+    enum ArmorType
+    {
+        NO_ARMOR,
+        CLOTH,
+        LEATHER,
+        MAIL,
+        PLATE,
+    };
+}
+
+namespace WeaponType
+{
+    enum WeaponType
+    {
+        NO_WEAPON,
+        ONEHAND_SWORD,
+        TWOHAND_SWORD,
+        DAGGER,
+        STAFF,
+        ONEHAND_CLUB,
+        TWOHAND_CLUB,
+        WAND,
+        SPELLBOOK, // offhand spellbooks for caster classes.
+        ONEHAND_MACE,
+        TWOHAND_MACE,
+        ONEHAND_AXE,
+        TWOHAND_AXE,
+        BOW,
+        SLINGSHOT,
+        CROSSBOW,
+        SHIELD,
+    };
+}
+
+namespace ItemQuality
+{
+    enum ItemQuality
+    {
+        POOR,
+        NORMAL,
+        ENHANCED,
+        RARE,
+        LORE,
+    };
 }
 
 class Item
 {
 	public:
-		Item( std::string name, size_t sizeX, size_t sizeY, std::string symbolFile, EquipPosition::EquipPosition equipPosition );
+		Item( std::string name, size_t sizeX, size_t sizeY, std::string symbolFile,
+		ItemQuality::ItemQuality itemQuality,
+		EquipPosition::EquipPosition equipPosition,
+		ItemType::ItemType itemType,
+		ArmorType::ArmorType armorType,
+		WeaponType::WeaponType weaponType );
 		// probably like CCharacter: baseOnType
-		
+
 		std::string getName() const;
-		
+        std::string getDescription() const;
+        void setDescription( std::string description );
+
+        int16_t getStrength() const;
+        int16_t getDexterity() const;
+        int16_t getIntellect() const;
+        int16_t getVitality() const;
+        int16_t getWisdom() const;
+        int16_t getHealth() const;
+        int16_t getMana() const;
+        int16_t getEnergy() const;
+        int16_t getArmor() const;
+
+        uint8_t getMinDamage() const;
+        uint8_t getMaxDamage() const;
+
+        void setStrength( int16_t strength );
+		void setDexterity( int16_t dexterity );
+		void setIntellect( int16_t intellect );
+		void setVitality( int16_t vitality );
+		void setWisdom( int16_t wisdom );
+		void setHealth( int16_t health );
+		void setMana( int16_t mana );
+		void setEnergy( int16_t energy );
+		void setArmor( int16_t armor );
+        void setMinDamage( uint8_t minDamage );
+        void setMaxDamage( uint8_t maxDamage );
+
 		size_t getSizeX() const;
 		size_t getSizeY() const;
-		
-		CTexture* getSymbolTexture();
-		
+
+        ItemQuality::ItemQuality getItemQuality() const;
+		ItemSlot::ItemSlot getItemSlot() const;
+		ItemType::ItemType getItemType() const;
+		ArmorType::ArmorType getArmorType() const;
+		WeaponType::WeaponType getWeaponType() const;
 		EquipPosition::EquipPosition getEquipPosition() const;
-		
+
+		std::string getArmorTypeText() const;
+		std::string getWeaponTypeText() const;
+		std::string getEquipPositionText() const;
+
+		CTexture* getSymbolTexture();
+
 	private:
 		std::string name;
+		std::string description;
+
 		size_t sizeX;
 		size_t sizeY;
-		
-		CTexture symbolTexture;
-		
+
+		ItemQuality::ItemQuality itemQuality;
+		ItemSlot::ItemSlot itemSlot;
+		ItemType::ItemType itemType;
+		ArmorType::ArmorType armorType;
+		WeaponType::WeaponType weaponType;
 		EquipPosition::EquipPosition equipPosition;
+
+		int16_t strengthModifier;
+        int16_t dexterityModifier;
+        int16_t intellectModifier;
+        int16_t vitalityModifier;
+        int16_t wisdomModifier;
+        int16_t healthModifier;
+        int16_t manaModifier;
+        int16_t energyModifier;
+        int16_t armorModifier;
+
+        uint8_t minDamage;
+        uint8_t maxDamage;
+
+		CTexture symbolTexture;
 };
 
 #endif // __ITEM_H__
