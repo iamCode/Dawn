b574e98c5e74b78a4faaa2e0708683bbbe07c81c
arnestig <arnestig@b4d8fe5f-8a90-43c9-9084-dcb629e63849>
2011-03-11 23:09:04 +0000

    * Removing NPCs for the new quest since they don't do anything useful yet. ;)

--- data/quests_thesylphirantidote.lua
+++ data/quests_thesylphirantidote.lua
@@ -6,15 +6,15 @@ end
 
 function quest_thesylphirantidote.init()
 	-- this quest is initialized from zone zone1
-	quest_thesylphirantidote.effreyLongback = DawnInterface.addMobSpawnPoint( "Human", -1510, 500, 1, 0 );
-        quest_thesylphirantidote.effreyLongback:setAttitude( Attitude.FRIENDLY );
-        quest_thesylphirantidote.effreyLongback:setWanderRadius(50);
-        quest_thesylphirantidote.effreyLongback:setName( "Effrey Longback" );
+	--quest_thesylphirantidote.effreyLongback = DawnInterface.addMobSpawnPoint( "Human", -1510, 500, 1, 0 );
+        --quest_thesylphirantidote.effreyLongback:setAttitude( Attitude.FRIENDLY );
+        --quest_thesylphirantidote.effreyLongback:setWanderRadius(50);
+        --quest_thesylphirantidote.effreyLongback:setName( "Effrey Longback" );
 
-	quest_thesylphirantidote.jemma = DawnInterface.addMobSpawnPoint( "Human", -2162, -4580, 1, 0 );
-	quest_thesylphirantidote.jemma:setAttitude( Attitude.FRIENDLY );
-        quest_thesylphirantidote.jemma:setWanderRadius(50);
-	quest_thesylphirantidote.jemma:setName( "Jemma" );
+	--quest_thesylphirantidote.jemma = DawnInterface.addMobSpawnPoint( "Human", -2162, -4580, 1, 0 );
+	--quest_thesylphirantidote.jemma:setAttitude( Attitude.FRIENDLY );
+        --quest_thesylphirantidote.jemma:setWanderRadius(50);
+	--quest_thesylphirantidote.jemma:setName( "Jemma" );
 	local sylphirwoodsEnter = DawnInterface.addInteractionRegion();
 	sylphirwoodsEnter:setPosition( -1215, -1000, 100, 360 );
 	sylphirwoodsEnter:setOnEnterText( "quest_thesylphirantidote.enterWoods()" );
