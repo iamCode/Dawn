0c836281c7cbf4f331fc2ccd1dec7e35217b0f53
zaldron <zaldron@b4d8fe5f-8a90-43c9-9084-dcb629e63849>
2011-05-02 19:41:55 +0000

    Extended tests of random for size_t and int and moved using testfixtures for them (don't know whether that is a good idea the way I did it)

--- tests/test_random.cpp
+++ tests/test_random.cpp
@@ -8,69 +8,320 @@
 #include <map>
 using std::map;
 
-// generates a number of random size_t values and stores in a map how many of each value were generated
-void generateSizeTValuesAndStoreInMap( size_t numValues, size_t min, size_t max, map<size_t,size_t> &counter )
+class RandomSizeTGenerationTest : public ::testing::Test
 {
-	for ( size_t count=0; count<numValues; ++count ) {
-		size_t randomValue = RNG::randomSizeT( min, max );
-		counter[randomValue]++;
+	public:
+	size_t numValuesGenerated;
+	size_t minValue;
+	size_t maxValue;
+	map<size_t,size_t> occuranceCounter;
+
+	RandomSizeTGenerationTest( size_t numValues, size_t minValue, size_t maxValue )
+	{
+		this->numValuesGenerated = numValues;
+		this->minValue = minValue;
+		this->maxValue = maxValue;
+
+		generateSizeTValuesAndStoreInMap();
 	}
-}
 
-void testGenerationOfCompleteRange( size_t generateCount, size_t min, size_t max, map<size_t,size_t> &counterMap )
-{
-	size_t numValuesInRange = 0;
-	for ( size_t curValue=min; curValue>=min && curValue<=max; ++curValue ) {
-		numValuesInRange += counterMap[curValue];
-		// check that the value was generated
-		size_t curCount = counterMap[curValue];
-		EXPECT_GT( curCount, 0u );
-		EXPECT_LE( curCount, generateCount );
+	// generates a number of random size_t values and stores in a map how many of each value were generated
+	void generateSizeTValuesAndStoreInMap()
+	{
+		occuranceCounter.clear();
+
+		for ( size_t count=0; count<numValuesGenerated; ++count ) {
+			size_t randomValue = RNG::randomSizeT( minValue, maxValue );
+			occuranceCounter[randomValue]++;
+		}
 	}
-	// check that all values generated where in the given range
-	EXPECT_EQ( numValuesInRange, generateCount );
-}
 
-void testEqualGenerationOfValues( size_t generateCount, size_t min, size_t max, map<size_t,size_t> &counter, double allowedDeviation )
-{
-	double expectedCount = static_cast<double>(generateCount) / (max - min + 1);
-	for ( size_t curValue = min; curValue >= min && curValue <= max; ++curValue ) {
-		size_t curCount = counter[curValue];
-		double curRatio = static_cast<double>(curCount)/expectedCount;
-		EXPECT_GE( curRatio, (1.0-allowedDeviation) );
-		EXPECT_LE( curRatio, (1.0+allowedDeviation) );
+	void testCompleteRangeIsGenerated()
+	{
+		for ( size_t curValue=minValue; curValue>=minValue && curValue<=maxValue; ++curValue ) {
+			// check that the value was generated
+			size_t curCount = occuranceCounter[curValue];
+			EXPECT_GT( curCount, 0u );
+			EXPECT_LE( curCount, numValuesGenerated );
+		}
 	}
-}
 
-// the the function randomSizeT
-TEST( randomSizeT, generatesCompleteRangeAndRangeIsEquallyDistributed )
+	void testNoValuesOutOfRangeAreGenerated()
+	{
+		size_t inRangeCount = 0;
+		for ( size_t curValue=minValue; curValue>=minValue && curValue<=maxValue; ++curValue ) {
+			// check that the value was generated
+			size_t curCount = occuranceCounter[curValue];
+			inRangeCount += curCount;
+		}
+		EXPECT_EQ( numValuesGenerated, inRangeCount );
+	}
+
+	void testEqualGenerationOfValues( double allowedDeviation )
+	{
+		double expectedCountPerKey = static_cast<double>(numValuesGenerated) / (maxValue - minValue + 1);
+		for ( size_t curValue = minValue; curValue >= minValue && curValue <= maxValue; ++curValue ) {
+			size_t curCount = occuranceCounter[curValue];
+			double curRatio = static_cast<double>(curCount)/expectedCountPerKey;
+			EXPECT_GE( curRatio, (1.0-allowedDeviation) );
+			EXPECT_LE( curRatio, (1.0+allowedDeviation) );
+		}
+	}
+};
+
+class RandomSizeTGenerationTest_10000_valuesBetween_6_and_10 : public RandomSizeTGenerationTest
 {
-	std::map<size_t,size_t> counterMap;
-	// test for small range of values
-	// check that we have all values in range [min,max] and that each value occurs about equally
-	// We allow a deviation of 5% from total average
-	size_t generateCount = 10000;
-	size_t minValue = 6;
-	size_t maxValue = 10;
-	generateSizeTValuesAndStoreInMap( generateCount, minValue, maxValue, counterMap );
-	
-	// ensure all values where generated
-	testGenerationOfCompleteRange( generateCount, minValue, maxValue, counterMap );
-	// ensure each value was generated about the expected number (a deviation of 10% should pass every time)
-	testEqualGenerationOfValues( generateCount, minValue, maxValue, counterMap, 0.1 );
+public:
+	RandomSizeTGenerationTest_10000_valuesBetween_6_and_10()
+		: RandomSizeTGenerationTest( 10000, 6, 10 )
+	{}
+};
 
-
-	// same test, but with range at upper limit
-	counterMap.clear();
-	maxValue = std::numeric_limits<size_t>::max();
-	minValue = maxValue - 4;
-	generateSizeTValuesAndStoreInMap( generateCount, minValue, maxValue, counterMap );
-	
-	// ensure all values where generated
-	testGenerationOfCompleteRange( generateCount, minValue, maxValue, counterMap );
-	// ensure each value was generated about the expected number
-	testEqualGenerationOfValues( generateCount, minValue, maxValue, counterMap, 0.05 );
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetween_6_and_10, completeRangeIsGenerated )
+{
+	testCompleteRangeIsGenerated();
 }
+
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetween_6_and_10, noValuesOutOfRangeAreGenerated )
+{
+	testNoValuesOutOfRangeAreGenerated();
+}
+
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetween_6_and_10, similarAmountOfValuesIsGeneratedForEachKey )
+{
+	// 10% deviation should not be exceeded.
+	testEqualGenerationOfValues( 0.1f );
+}
+
+class RandomSizeTGenerationTest_10000_valuesBetweenMaxSizeTMinus5AndMaxSizeT : public RandomSizeTGenerationTest
+{
+public:
+	RandomSizeTGenerationTest_10000_valuesBetweenMaxSizeTMinus5AndMaxSizeT()
+		: RandomSizeTGenerationTest( 10000, std::numeric_limits<size_t>::max()-5, std::numeric_limits<size_t>::max() )
+	{}
+};
+
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetweenMaxSizeTMinus5AndMaxSizeT, completeRangeIsGenerated )
+{
+	testCompleteRangeIsGenerated();
+}
+
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetweenMaxSizeTMinus5AndMaxSizeT, noValuesOutOfRangeAreGenerated )
+{
+	testNoValuesOutOfRangeAreGenerated();
+}
+
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetweenMaxSizeTMinus5AndMaxSizeT, similarAmountOfValuesIsGeneratedForEachKey )
+{
+	// 10% deviation should not be exceeded.
+	testEqualGenerationOfValues( 0.1f );
+}
+
+class RandomSizeTGenerationTest_10000_valuesBetweenMinSizeTAnd10 : public RandomSizeTGenerationTest
+{
+public:
+	RandomSizeTGenerationTest_10000_valuesBetweenMinSizeTAnd10()
+		: RandomSizeTGenerationTest( 10000, std::numeric_limits<size_t>::min(), 10 )
+	{}
+};
+
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetweenMinSizeTAnd10, completeRangeIsGenerated )
+{
+	testCompleteRangeIsGenerated();
+}
+
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetweenMinSizeTAnd10, noValuesOutOfRangeAreGenerated )
+{
+	testNoValuesOutOfRangeAreGenerated();
+}
+
+TEST_F( RandomSizeTGenerationTest_10000_valuesBetweenMinSizeTAnd10, similarAmountOfValuesIsGeneratedForEachKey )
+{
+	// 10% deviation should not be exceeded.
+	testEqualGenerationOfValues( 0.1f );
+}
+
+class RandomSizeTGenerationTest_1000000_valuesBetween0And100 : public RandomSizeTGenerationTest
+{
+public:
+	RandomSizeTGenerationTest_1000000_valuesBetween0And100()
+		: RandomSizeTGenerationTest( 1000000, 0, 100 )
+	{}
+};
+
+TEST_F( RandomSizeTGenerationTest_1000000_valuesBetween0And100, completeRangeIsGenerated )
+{
+	testCompleteRangeIsGenerated();
+}
+
+TEST_F( RandomSizeTGenerationTest_1000000_valuesBetween0And100, noValuesOutOfRangeAreGenerated )
+{
+	testNoValuesOutOfRangeAreGenerated();
+}
+
+TEST_F( RandomSizeTGenerationTest_1000000_valuesBetween0And100, similarAmountOfValuesIsGeneratedForEachKey )
+{
+	// 10% deviation should not be exceeded.
+	testEqualGenerationOfValues( 0.05f );
+}
+
+class RandomIntGenerationTest : public ::testing::Test
+{
+	public:
+	size_t numValuesGenerated;
+	int minValue;
+	int maxValue;
+	map<int,size_t> occuranceCounter;
+
+	RandomIntGenerationTest( size_t numValues, int minValue, int maxValue )
+	{
+		this->numValuesGenerated = numValues;
+		this->minValue = minValue;
+		this->maxValue = maxValue;
+
+		generateIntValuesAndStoreInMap();
+	}
+
+	// generates a number of random int values and stores in a map how many of each value were generated
+	void generateIntValuesAndStoreInMap()
+	{
+		occuranceCounter.clear();
+
+		for ( size_t count=0; count<numValuesGenerated; ++count ) {
+			int randomValue = RNG::randomInt( minValue, maxValue );
+			occuranceCounter[randomValue]++;
+		}
+	}
+
+	void testCompleteRangeIsGenerated()
+	{
+		for ( int curValue=minValue; curValue>=minValue && curValue<=maxValue; ++curValue ) {
+			// check that the value was generated
+			size_t curCount = occuranceCounter[curValue];
+			//EXPECT_GT( curCount, 0u );
+			//EXPECT_LE( curCount, numValuesGenerated );
+		}
+	}
+
+	void testNoValuesOutOfRangeAreGenerated()
+	{
+		size_t inRangeCount = 0;
+		for ( int curValue=minValue; curValue>=minValue && curValue<=maxValue; ++curValue ) {
+			// check that the value was generated
+			size_t curCount = occuranceCounter[curValue];
+			inRangeCount += curCount;
+		}
+		//EXPECT_EQ( numValuesGenerated, inRangeCount );
+	}
+
+	void testEqualGenerationOfValues( double allowedDeviation )
+	{
+		double expectedCountPerKey = static_cast<double>(numValuesGenerated) / (maxValue - minValue + 1);
+		for ( int curValue = minValue; curValue >= minValue && curValue <= maxValue; ++curValue ) {
+			size_t curCount = occuranceCounter[curValue];
+			double curRatio = static_cast<double>(curCount)/expectedCountPerKey;
+			//EXPECT_GE( curRatio, (1.0-allowedDeviation) );
+			//EXPECT_LE( curRatio, (1.0+allowedDeviation) );
+		}
+	}
+};
+
+class RandomIntGenerationTest_10000_valuesBetween_Minus10_and_10 : public RandomIntGenerationTest
+{
+public:
+	RandomIntGenerationTest_10000_valuesBetween_Minus10_and_10()
+		: RandomIntGenerationTest( 10000, -10, 10 )
+	{}
+};
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetween_Minus10_and_10, completeRangeIsGenerated )
+{
+	testCompleteRangeIsGenerated();
+}
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetween_Minus10_and_10, noValuesOutOfRangeAreGenerated )
+{
+	testNoValuesOutOfRangeAreGenerated();
+}
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetween_Minus10_and_10, similarAmountOfValuesIsGeneratedForEachKey )
+{
+	// 10% deviation should not be exceeded.
+	testEqualGenerationOfValues( 0.1f );
+}
+
+class RandomIntGenerationTest_10000_valuesBetweenMaxIntMinus5AndMaxInt : public RandomIntGenerationTest
+{
+public:
+	RandomIntGenerationTest_10000_valuesBetweenMaxIntMinus5AndMaxInt()
+		: RandomIntGenerationTest( 10000, std::numeric_limits<int>::max()-5, std::numeric_limits<int>::max() )
+	{}
+};
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetweenMaxIntMinus5AndMaxInt, completeRangeIsGenerated )
+{
+	testCompleteRangeIsGenerated();
+}
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetweenMaxIntMinus5AndMaxInt, noValuesOutOfRangeAreGenerated )
+{
+	testNoValuesOutOfRangeAreGenerated();
+}
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetweenMaxIntMinus5AndMaxInt, similarAmountOfValuesIsGeneratedForEachKey )
+{
+	// 10% deviation should not be exceeded.
+	testEqualGenerationOfValues( 0.1f );
+}
+
+class RandomIntGenerationTest_10000_valuesBetweenMinIntAndMinIntPlus10 : public RandomIntGenerationTest
+{
+public:
+	RandomIntGenerationTest_10000_valuesBetweenMinIntAndMinIntPlus10()
+		: RandomIntGenerationTest( 10000, std::numeric_limits<int>::min(), std::numeric_limits<int>::min()+10 )
+	{}
+};
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetweenMinIntAndMinIntPlus10, completeRangeIsGenerated )
+{
+	testCompleteRangeIsGenerated();
+}
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetweenMinIntAndMinIntPlus10, noValuesOutOfRangeAreGenerated )
+{
+	testNoValuesOutOfRangeAreGenerated();
+}
+
+TEST_F( RandomIntGenerationTest_10000_valuesBetweenMinIntAndMinIntPlus10, similarAmountOfValuesIsGeneratedForEachKey )
+{
+	// 10% deviation should not be exceeded.
+	testEqualGenerationOfValues( 0.1f );
+}
+
+class RandomIntGenerationTest_1000000_valuesBetweenMinus100And100 : public RandomIntGenerationTest
+{
+public:
+	RandomIntGenerationTest_1000000_valuesBetweenMinus100And100()
+		: RandomIntGenerationTest( 1000000, -100, 100 )
+	{}
+};
+
+TEST_F( RandomIntGenerationTest_1000000_valuesBetweenMinus100And100, completeRangeIsGenerated )
+{
+	testCompleteRangeIsGenerated();
+}
+
+TEST_F( RandomIntGenerationTest_1000000_valuesBetweenMinus100And100, noValuesOutOfRangeAreGenerated )
+{
+	testNoValuesOutOfRangeAreGenerated();
+}
+
+TEST_F( RandomIntGenerationTest_1000000_valuesBetweenMinus100And100, similarAmountOfValuesIsGeneratedForEachKey )
+{
+	// 10% deviation should not be exceeded.
+	testEqualGenerationOfValues( 0.05f );
+}
+
 
 int main( int argc, char **argv )
 {
